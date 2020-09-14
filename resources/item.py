796404b85ffa02c26ac62bf15a38bde1a28d3565
fredrikolsson96 <fredrik.olsson967@gmail.com>
2020-09-14 15:16:50 +0200

    minor fixes

--- resources/item.py
+++ resources/item.py
@@ -36,7 +36,7 @@ class Item(Resource):
     def post(self, name):
         # check if an item with that name already exists
         if ItemModel.find_by_name(name):
-            return {'message': f'An item with name {name} already exists'}, 400
+            return {'message': 'An item with name {} already exists'.format(name)}, 400
 
         # get only the parser arguments from the JSON payload
         data = Item.parser.parse_args()
